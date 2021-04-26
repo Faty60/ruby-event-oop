ae6f8e1dc03856f3f886e232401f4d7a494baaf6
Faty60 <sidib.fatou2@gmail.com>
2021-04-27 00:55:37 +0200

    first commit of the ruby-event-opp

--- app.rb
+++ app.rb
@@ -10,3 +10,21 @@ require_relative 'lib/event'
 
 # Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
 # Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
+julie = User.new("julie@gmail.com", 31)
+kadi = User.new("kadi@gmail.com", 33)
+pierre = User.new("pierre@gmail.com", 45)
+ramata = User.new("assata@gmail.com", 32)
+diama = User.new("diama@gmail.com", 32)
+binta = User.new("sidib.ftou2@gmail.com", 31)
+
+
+
+#my_event = Event.new(Time.parse("2021-04-26 16:30"), 30, "standup quotidien", ["julie@gmail.com", "kadi@gmail.com", "pierre@gmail.com"])
+#user1 = User.find_by_email("julie@gmail.com")
+
+
+#puts my_event.to_s
+puts kadi.email
+user1 = User.find_by_email("julie@gmail.com")
+puts "Voici l'age du User trouvé : #{user1.age}"
+
--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,63 @@
-class Event
+#require "pry"
+require "time"
 
+class Event
+    attr_accessor :start_date, :duration, :title, :attendees
+    @@user_count = 0
+
+    def initialize(starting_date, duration_of_event, title_of_event, participant)
+        @start_date = starting_date
+        @duration = duration_of_event
+        @title = title_of_event
+        @attendees = participant
+        @@user_count = @@user_count + 1
+    end 
+
+#Décaler de 24h
+    def postpone_24h
+        return @start_date += 86400
+    end 
+# Calculer la date de fin
+    def end_date
+        return @start_date = @start_date + @duration
+    end
+# Savoir si un évènement est passé ou pas
+
+    def is_past?
+        return Time.now > @start_date 
+    end 
+
+    def future?
+        return Time.now < @start_date 
+    end
+
+    def is_soon?
+        return @start_date < Time.now + 1800
+    end
+
+    def to_s
+        puts "> Titre : #{self.read_title}" 
+        puts "> Date de début : #{start_date}"
+        puts "> Durée  : #{duration} minutes"
+        print "> Invités : "
+        attendees.length.times do |index|
+            if index > 0 
+                print ", "
+            end
+            print "#{attendees[index]}"
+        end
+        puts
+    end 
+
+    def read_title
+        return @title
+    end
+
+    def self.all
+        return @@user_count
+    end
 end
+
+
+#binding.pry
+#puts "end of file"
